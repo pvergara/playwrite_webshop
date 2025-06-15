a61663e640afbd4933a5724eb6092374084ebdc8
pvergara <ecos.logic.org@gmail.com>
2025-06-15 22:17:01 +0200

    Initial commit

---
+++ tests/webshop.spec.ts
@@ -0,0 +1,31 @@
+import {test, expect, Page, Locator} from '@playwright/test';
+
+async function rejectCookieWall(page:Page) {
+    await page.getByRole('button', {name: 'Manage options'}).click();
+
+    const sliderLocatorCollection = page.locator('label > span.fc-preference-slider');
+    if(await sliderLocatorCollection.count() === 0)
+        return;
+
+    const number = await sliderLocatorCollection.count();
+
+    for (let i = 0; i < number;i++) {
+        const sliderLocator = sliderLocatorCollection.nth(i);
+
+        const inputLocator = sliderLocatorCollection.nth(i).locator('input[type=checkbox]');
+
+        if (await inputLocator.isChecked() && await sliderLocator.isVisible())
+            await sliderLocator.click()
+    }
+    await page.getByRole('button', { name: 'Confirm choices' }).click();
+}
+
+
+test('has title', async ({page}) => {
+    await page.goto('https://automationexercise.com/category_products');
+
+    await rejectCookieWall(page);
+
+    await page.locator('.productinfo >> nth=0').hover();
+
+});
